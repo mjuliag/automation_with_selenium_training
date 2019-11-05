192f5d6e41bb84463d1899e2f93540dfe2e4b8f2
Julia Girona <v-jgirona@youngliving.com>
2019-11-05 12:20:46 -0300

    Resolves how to save screenshots. In your face, Java

--- src/com/company/Screenshots.java
+++ src/com/company/Screenshots.java
@@ -8,64 +8,71 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.concurrent.TimeUnit;
 
 public class Screenshots {
-        private WebDriver driver;
-        private String baseUrl;
+    private WebDriver driver;
+    private String baseUrl;
 
-        @Before
-        public void setUp() throws Exception {
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\JuliaGirona\\chromedriver_win32\\chromedriver.exe");
-            driver = new ChromeDriver();
-            baseUrl = "https://www.expedia.com/";
-
-            // Maximize the browser's window
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        }
-
-        @Test
-        public void testScreenshots() throws Exception {
-            driver.get(baseUrl);
-            driver.findElement(By.id("tab-flight-tab")).click();
-
-            // Find Elements
-            WebElement flight_origin = driver.findElement(By.id("flight-origin"));
-            WebElement flight_destination = driver.findElement(By.id("flight-destination"));
-            WebElement departure_date = driver.findElement(By.id("flight-departing"));
-            WebElement return_date = driver.findElement(By.id("flight-returning"));
-            WebElement search = driver.findElement(By.id("search-button"));
-
-            // Send data to elements
-            flight_origin.sendKeys("New York");
-            departure_date.sendKeys("09/03/2014");
-            return_date.clear();
-            return_date.sendKeys("09/10/2014");
-            flight_destination.sendKeys("New York");
-            search.click();
-        }
-
-        public static String getRandomString(int length) {
-            StringBuilder sb = new StringBuilder();
-            String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-            for (int i = 0; i < length; i++) {
-                int index = (int) (Math.random() * characters.length());
-                sb.append(characters.charAt(index));
-            }
-            return sb.toString();
-        }
-
-        @After
-        public void tearDown() throws Exception {
-            String fileName = getRandomString(10) + ".png";
-            String directory = "C:\\Users\\Public\\Desktop";
-
-            File sourceFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-
-            //TODO find a new way to copy files!!!!!
-            //FileUtils.copyFile(sourceFile, new File(directory + fileName));
-            driver.quit();
-        }
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\JuliaGirona\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        baseUrl = "https://www.expedia.com/";
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
+
+    @Test
+    public void testScreenshots() throws Exception {
+        driver.get(baseUrl);
+        driver.findElement(By.id("tab-flight-tab-hp")).click();
+
+        // Find Elements
+        WebElement flight_origin = driver.findElement(By.id("flight-origin-hp-flight"));
+        WebElement flight_destination = driver.findElement(By.id("flight-destination-hp-flight"));
+        WebElement departure_date = driver.findElement(By.id("flight-departing-hp-flight"));
+        WebElement return_date = driver.findElement(By.id("flight-returning-hp-flight"));
+        WebElement search = driver.findElement(By.xpath("//*[@id=\"gcw-flights-form-hp-flight\"]/div[7]/label/button"));
+
+        // Send data to elements
+        flight_origin.sendKeys("New York");
+        departure_date.sendKeys("09/11/2019");
+        return_date.clear();
+        return_date.sendKeys("09/11/2019");
+        flight_destination.sendKeys("New York");
+        search.click();
+    }
+
+    public static String getRandomString(int length) {
+        StringBuilder sb = new StringBuilder();
+        String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        for (int i = 0; i < length; i++) {
+            int index = (int) (Math.random() * characters.length());
+            sb.append(characters.charAt(index));
+        }
+        return sb.toString();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        String fileName = getRandomString(10) + ".jpg";
+        String newDirectory = "C:\\Users\\JuliaGirona\\Desktop\\";
+
+        File sourceFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File destination = new File(newDirectory + fileName);
+        
+        Path path = Files.copy(sourceFile.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        if (Files.exists(path)) {
+            System.out.println("File copied succesfully to " + destination);
+        } else {
+            System.out.println("Unable to copy file :(");
+        }
+
+        driver.quit();
+    }
+}
 
